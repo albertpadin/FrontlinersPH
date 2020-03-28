e66683c342a1dd287ba73652e98dbac6c834af56
Arnelle Balane <arnellebalane@gmail.com>
2020-03-28 21:57:01 +0800

    Create client-only location detail page

---
+++ src/templates/location.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const LocationTemplate = ({ location }) => {
+  const [_, id] = /\/location\/(\w+)/;
+
+  return <div>loading location data</div>;
+};
+
+export default LocationTemplate;
