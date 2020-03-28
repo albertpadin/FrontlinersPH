c5fce6fa9ed878b04821e8bdebd5c7aefdb9fb8b
Arnelle Balane <arnellebalane@gmail.com>
2020-03-28 22:51:10 +0800

    Fetch location data from Firestore

--- src/templates/location.js
+++ src/templates/location.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { navigate } from 'gatsby';
+import firebase from 'gatsby-plugin-firebase';
 
 const LocationTemplate = ({ location }) => {
-  const [_, id] = /\/location\/(\w+)/;
+  const [data, setData] = useState(null);
+  const [_, id] = location.pathname.match(/\/location\/(\w+)/);
+
+  useEffect(() => {
+    (async () => {
+      if (!id) {
+        return navigate('/404');
+      }
+
+      const locationSnapshot = await firebase
+        .firestore()
+        .doc(`locations/${id}`)
+        .get();
+      const locationData = locationSnapshot.data();
+      if (!locationData) {
+        return navigate('/404');
+      }
+
+      setData(locationData);
+      console.log(locationData);
+    })();
+  }, [firebase, id]);
 
   return <div>loading location data</div>;
 };
