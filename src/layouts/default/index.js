ee22d253a578f9da23d9638b8cb07c6668c580cd
Arnelle Balane <arnellebalane@gmail.com>
2020-03-28 12:49:14 +0800

    Store current Firebase user in FirebaseUserContext

--- src/layouts/default/index.js
+++ src/layouts/default/index.js
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import firebase from 'gatsby-plugin-firebase';
 
 import '@styles/global.css';
 import Header from '@components/header';
 import style from './styles.module.css';
 import { SiteTitleQuery } from '@src/queries';
+import { FirebaseUserContext } from '@src/contexts';
 
 const Layout = ({ children }) => {
   const data = SiteTitleQuery();
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged(setUser);
+  }, [firebase]);
 
   return (
-    <>
+    <FirebaseUserContext.Provider value={user}>
       <Header siteTitle={data.site.siteMetadata.title} />
       <div className={style.wrapper}>
         <main>{children}</main>
@@ -19,7 +26,7 @@ const Layout = ({ children }) => {
           <a href="https://www.gatsbyjs.org">Gatsby</a>
         </footer>
       </div>
-    </>
+    </FirebaseUserContext.Provider>
   );
 };
 
