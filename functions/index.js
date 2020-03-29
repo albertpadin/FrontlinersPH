df64e92680d90dad530f9aaff802e8576679be08
Arnelle Balane <arnellebalane@gmail.com>
2020-03-29 12:44:16 +0800

    Modify commitmentRevisions function to set location statistics

--- functions/index.js
+++ functions/index.js
@@ -1,15 +1,39 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const has = require('lodash/has');
+const set = require('lodash/set');
 
 admin.initializeApp();
+const db = admin.firestore();
 
 exports.commitmentRevisions = functions.firestore
   .document('commitments/{cid}/revisions/{rid}')
   .onCreate(async snapshot => {
     // Get the parent commitment document from the revision sub-document.
     const commitmentRef = snapshot.ref.parent.parent;
-    const commitmentData = (await commitmentRef.get()).data() || {};
     const revisionData = snapshot.data();
 
-    await commitmentRef.set({ ...commitmentData, ...revisionData });
+    await db.runTransaction(async t => {
+      const commitmentType = revisionData.data.type;
+
+      const locationId = revisionData.data.location;
+      const locationRef = db.doc(`locations/${locationId}`);
+      const locationSnapshot = await locationRef.get();
+      const locationData = locationSnapshot.data();
+
+      // Update statistics for this commitment type
+      let statistics = locationData.statistics;
+      if (!has(statistics, commitmentType)) {
+        statistics = set(statistics, commitmentType, {
+          requests: 0,
+          commitments: 0,
+        });
+      }
+      statistics[commitmentType].commitments += Number(
+        revisionData.data.quantity
+      );
+
+      await commitmentRef.set(revisionData);
+      await locationRef.update({ statistics });
+    });
   });
