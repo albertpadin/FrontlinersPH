700567165244b7577792e79d14056324c12d7acf
Arnelle Balane <arnellebalane@gmail.com>
2020-03-29 11:46:56 +0800

    Define Cloud Function to update commitment latest revision

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,15 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+exports.commitmentRevisions = functions.firestore
+  .document('commitments/{cid}/revisions/{rid}')
+  .onCreate(async snapshot => {
+    // Get the parent commitment document from the revision sub-document.
+    const commitmentRef = snapshot.ref.parent.parent;
+    const commitmentData = (await commitmentRef.get()).data() || {};
+    const revisionData = snapshot.data();
+
+    await commitmentRef.set({ ...commitmentData, ...revisionData });
+  });
