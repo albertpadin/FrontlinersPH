723a82701bb6c3b35406a49381dd2f8e5188c5e4
Mark Robin <markrobin815@gmail.com>
2020-03-29 14:48:46 +0800

    Implement creating new locations

--- src/components/location-form-modal/index.js
+++ src/components/location-form-modal/index.js
@@ -5,67 +5,105 @@ import {
   Modal,
   ModalHeader,
   ModalBody,
-  ModalFooter,
-  Input,
   Label,
-  Form,
-  FormGroup,
   Row,
   Col,
 } from 'reactstrap';
-
-import style from './styles.module.css';
+import {
+  AvForm,
+  AvField,
+  AvGroup,
+  AvInput,
+  AvFeedback,
+} from 'availity-reactstrap-validation';
+import firebase from 'gatsby-plugin-firebase';
 
 const LocationFormModal = ({ isShow, toggle }) => {
+  const addLocation = data => {
+    console.log(data);
+    return firebase
+      .firestore()
+      .collection('locations')
+      .add({
+        name: data.name,
+        type: data.type,
+        address: {
+          city: data.address.city,
+          province: data.address.province,
+        },
+      })
+      .then(() => {
+        toggle();
+      })
+      .catch(error => {
+        console.error('Error writing firestore document: ', error);
+      });
+  };
+
+  const handleSubmit = (event, values) => {
+    addLocation(values);
+  };
+
   return (
     <Modal isOpen={isShow} toggle={toggle} size="lg">
       <ModalHeader toggle={toggle}>Add a Location</ModalHeader>
       <ModalBody>
-        <Form>
-          <FormGroup>
+        <AvForm onValidSubmit={handleSubmit}>
+          <AvGroup>
             <Label for="name">What’s the name of your location?</Label>
-            <Input type="text" name="name" placeholder="Enter location" />
-          </FormGroup>
-          <FormGroup>
+            <AvInput
+              type="text"
+              name="name"
+              placeholder="Enter location"
+              required
+            />
+            <AvFeedback>Please enter your location</AvFeedback>
+          </AvGroup>
+          <AvGroup>
             <Label for="name">What’s your location type?</Label>
-            <Input type="select" name="type">
+            <AvField type="select" name="type" required>
               <option disabled>Select location type</option>
-              <option>Hospital</option>
-              <option>Quarantine Area</option>
-            </Input>
-          </FormGroup>
+              <option value="HOSPITAL">Hospital</option>
+              <option value="PRODUCTION_HUB">Production Hub</option>
+              <option value="KITCHEN">Kitchen</option>
+              <option value="QUARANTINE_AREA">Quarantine Area</option>
+              <option value="FRONTLINERS_ACCOMMODATION">
+                Frontliners Accommodation
+              </option>
+            </AvField>
+            <AvFeedback>Please choose your location type</AvFeedback>
+          </AvGroup>
           <Row>
             <Col>
-              <FormGroup>
+              <AvGroup>
                 <Label for="city">What city/municipality are you from?</Label>
-                <Input
+                <AvInput
                   type="text"
-                  name="city"
+                  name="address.city"
                   placeholder="Enter city/municipality"
+                  required
                 />
-              </FormGroup>
+                <AvFeedback>Please enter your city</AvFeedback>
+              </AvGroup>
             </Col>
             <Col>
-              <FormGroup>
+              <AvGroup>
                 <Label for="province">What province do you live in?</Label>
-                <Input
+                <AvInput
                   type="text"
-                  name="province"
+                  name="address.province"
                   placeholder="Enter province"
+                  required
                 />
-              </FormGroup>
+                <AvFeedback>Please enter your province</AvFeedback>
+              </AvGroup>
             </Col>
           </Row>
-        </Form>
+          <Button color="primary" type="submit" block>
+            Submit Entry
+          </Button>
+        </AvForm>
       </ModalBody>
-      <ModalFooter>
-        <Button color="primary" onClick={toggle}>
-          Submit Entry
-        </Button>
-        <Button color="secondary" onClick={toggle}>
-          Cancel
-        </Button>
-      </ModalFooter>
     </Modal>
   );
 };
