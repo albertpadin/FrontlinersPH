f2f0b4cb5c96df245a18888af0ae29f4b1818752
King Joshua Montayre <kmontayre07@gmail.com>
2020-04-01 19:23:41 +0800

    Updated the supplies list based on the updated list in the spreadsheet

--- src/constants.js
+++ src/constants.js
@@ -1,22 +1,42 @@
 export const NEED_TYPE_CHOICES = [
   { value: 'AEROSOL_BOXES', label: 'Aerosol Boxes' },
-  { value: 'CASH', label: 'Cash' },
+  { value: 'ALCOHOL', label: 'Alcohol' },
+  { value: 'BATTERIS', label: 'Batteries' },
+  { value: 'BOOTIES', label: 'Booties' },
+  { value: 'BOUFFANT_HEAD_CAPS', label: 'Bouffant Head Caps' },
+  //{ value: 'CASH', label: 'Cash' }, NOT IN THE LIST
+  { value: 'CHLOROX_DISINFECTANT', label: 'Chlorox Disinfectant' },
+  { value: 'DETERGENT', label: 'Detergent' },
   { value: 'DISPOSABLE_GOGGLES', label: 'Disposable Goggles' },
   { value: 'ECOBAG_CLOTH', label: 'Ecobag Cloth' },
   { value: 'FACE_SHIELDS', label: 'Face Shields' },
   { value: 'HAND_SANITIZERS', label: 'Hand Sanitizers' },
+  { value: 'HANDWASHING_SOAP', label: 'Handwashing Soap' },
+  { value: 'HAZMAT_SUIT', label: 'Hazmat Suit'},
+  { value: 'HEAD_COVER', label: 'Head Cover'},
   { value: 'INFRARED_THERMOMETERS', label: 'Infrared Thermometers' },
-  { value: 'LAB_GOGGLES', label: 'Lab Goggles' },
-  { value: 'LAB_GOWNS', label: 'Lab Gowns' },
+  { value: 'ISOLATION_GOWNS', label: 'Isolation Gowns'},
+  { value: 'LAB/OPTICAL_GOGGLES', label: 'Lab/Optical Goggles' },
+  //{ value: 'LAB_GOWNS', label: 'Lab Gowns' }, NOT IN THE LIST
+  { value: 'LARYNGOSCOPE_ADULT', label: 'Laryngoscope Adult'},
   { value: 'LATEX_GLOVES', label: 'Latex Gloves' },
-  { value: 'MEALS', label: 'Meals' },
+  { value: 'LYSOL/DISINFECTANT', label: 'Lysol/Disinfectant' },
+  //{ value: 'MEALS', label: 'Meals' }, NOT IN THE LIST
+  { value: 'MEDICAL_BONNETS', label: 'Medical Bonnets' },
+  { value: 'MEDICINE', label: 'Medicine' },
   { value: 'N95_MASKS', label: 'N95 Masks' },
   { value: 'NITRATE_GLOVES', label: 'Nitrate Gloves' },
+  { value: 'OVERALL_SUITS', label: 'Overall Suits' },
+  { value: 'PARACETAMOL', label: 'Paracetamol' },
   { value: 'PPES', label: 'PPEs' },
-  { value: 'RAW_MATERIALS', label: 'Raw Materials' },
+  //{ value: 'RAW_MATERIALS', label: 'Raw Materials' }, NOT IN THE LIST
+  { value: 'SCRUB_SUIT', label: 'Scrub Suit' },
   { value: 'SHOE_COVERS', label: 'Shoe Covers' },
-  { value: 'SUITS', label: 'Suits' },
+  { value: 'STERILE_GLOVES', label: 'Sterile Gloves' },
+  { value: 'SURGICAL_GLOVES', label: 'Surgical Gloves' },
+  //{ value: 'SUITS', label: 'Suits' }, NOT IN THE LIST
   { value: 'SURGICAL_MASKS', label: 'Surgical Masks' },
+  { value: 'VITAMINS', label: 'Vitamins' },
   { value: 'OTHER', label: 'Other' },
 ];
 
