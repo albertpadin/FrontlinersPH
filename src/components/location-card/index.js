4ecfd46cbf4abdaab4cea138b33725e34df82630
Edbert Estevez <ejestevez26@gmail.com>
2020-04-03 17:59:46 +0800

    New homepage card locations

--- src/components/location-card/index.js
+++ src/components/location-card/index.js
@@ -1,60 +1,55 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import {
-  Button,
   Card,
   CardBody,
   CardImg,
   CardTitle,
   CardSubtitle,
+  CardFooter,
   Col,
   Progress,
   Row,
 } from 'reactstrap';
 import map from 'lodash/map';
-
+import format from 'date-fns/format';
 import { NEED_TYPE_CHOICES } from '@src/constants';
 import style from './styles.module.css';
-import placeholderImg from '../../images/placeholder-img.png';
+import placeholderImg from '../../images/placeholder-new.png';
+import ClockIcon from '@assets/svg/clock.svg';
+import ChevronIcon from '@assets/svg/chevron.svg';
 
 const LocationCardStats = ({ type, stats }) => {
   const label = NEED_TYPE_CHOICES.find(({ value }) => value === type).label;
   const progress = (stats.commitments / stats.requests) * 100;
 
-  const progressColor = () => {
-    if (progress <= 50) {
-      return 'danger';
-    } else if (progress <= 99) {
-      return 'warning';
-    } else {
-      return 'success';
-    }
-  };
   return (
-    <>
+    <React.Fragment>
       <Row className="d-flex justify-content-between">
         <Col className="text-left">
           <span className={style.statsTitle}>{label}</span>
         </Col>
       </Row>
       <Progress
+        style={{height: 10}}
         value={progress === Infinity ? 100 : progress}
-        color={progressColor()}
+        color={"#60A1EC"}
       />
       <Row>
         <Col className={`mt-1 text-right ${style.stats}`}>
-          {stats.commitments} commitments to {stats.requests} requests
+          {stats.commitments} donations to {stats.requests} requests
         </Col>
       </Row>
-    </>
+    </React.Fragment>
   );
 };
 
 const LocationCard = ({ location }) => {
-  const { id, author, created_at, data, statistics } = location;
+  const { id, created_at, data, statistics } = location;
+  const statCount = statistics ? Object.keys(statistics).length : 0;
 
   return (
-    <Card className="mt-5">
+    <Card className="mt-5" style={{borderRadius: 10}}>
       <div className="position-relative">
         <div className={`position-absolute ${style.locationDiv}`}>
           <CardTitle className={style.locationTitle}>{data.name}</CardTitle>
@@ -70,26 +65,42 @@ const LocationCard = ({ location }) => {
           alt="location image"
         />
       </div>
+
+      <div className={style.locationUpdate}>
+        <span className={style.clockIcon}>
+          <ClockIcon/>{' '}
+          <p className={style.locationStatus}>UPDATED</p>
+        </span>
+        <div className={style.locationTimestamp}>
+          {format(new Date().setTime(created_at.seconds * 1000), 'MMMM d, yyyy h:mm a')}
+        </div>
+      </div>
+
       <CardBody className={style.statsBody}>
+        <span className={style.statsCount}>
+          {`This location ${statCount === 1 ? 'need' : 'needs'}`}
+          <b>{`${statCount} ${statCount === 1 ? 'item' : 'items'}`}</b>
+        </span>
         <div className={`${style.statsDiv} text-center`}>
-          {location.statistics ? (
-            map(location.statistics, (stats, type) => (
+          {statistics ? (
+            map(statistics, (stats, type) => (
               <LocationCardStats key={type} type={type} stats={stats} />
             ))
           ) : (
             <small>No requests or donations made yet</small>
           )}
         </div>
-        <Button
-          tag={Link}
+        
+      </CardBody>
+      <CardFooter className={style.statsFooter}>
+        <Link
           to={`/location/${id}`}
-          color="primary"
-          className="mt-4"
-          block
+          className={style.statsLink}
         >
           View Efforts
-        </Button>
-      </CardBody>
+          <ChevronIcon/>
+        </Link>
+      </CardFooter>
     </Card>
   );
 };
